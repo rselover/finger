85c833b0542bd05c765d4922c56a85e5f0079381
Rob Selover <rselover@users.noreply.github.com>
2025-07-24 11:33:55 +0000

    Service Worker Path Issues

--- src/sw-register.js
+++ src/sw-register.js
@@ -1,6 +1,6 @@
 if (!navigator.serviceWorker.controller) {
 	//Register the ServiceWorker
-	navigator.serviceWorker.register('/sw.js', {
+	navigator.serviceWorker.register('sw.js', {
 		scope: './'
 	});
 }
